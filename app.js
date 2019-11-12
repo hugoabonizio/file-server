c347cbee068ac2c00dabaf0c6aa98071d0c1461b
Hugo Abonizio <hugo_abonizio@hotmail.com>
2019-11-12 14:07:05 -0300

    feat: Add configuration

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 const fs = require('fs')
 const http = require('http')
 const net = require('net')
+require('dotenv').config()
 
 if (!fs.existsSync('uploads/')) {
   fs.mkdirSync('uploads')
 }
 
-const HOST = process.env['HOST'] || 'localhost'
+const HOST = process.env.HOST || 'localhost'
+const SOCKET_PORT = process.env.SOCKET_PORT || 9999
+const HTTP_PORT = process.env.HTTP_PORT || 3000
 
 net.createServer((socket) => {
   socket.on('data', (data) => {
     const name = Math.random().toString(36).substring(2, 10)
 
-    socket.write(`http://${HOST}:8080/${name}\n`)
+    socket.write(`http://${HOST}:${HTTP_PORT}/${name}\n`)
     fs.writeFile(`uploads/${name}`, data, (err) => {
       if (err) {
         console.error('Error writing file', err)
@@ -25,7 +28,7 @@ net.createServer((socket) => {
   socket.on('error', (err) => {
     console.error('Socket error', err)
   })
-}).listen(9999, '127.0.0.1')
+}).listen(SOCKET_PORT, '127.0.0.1')
 
 const server = http.createServer((req, res) => {
   console.log(new Date(), req.url)
@@ -40,4 +43,4 @@ const server = http.createServer((req, res) => {
   })
 })
 
-server.listen(8080, () => console.log('Listening...'))
+server.listen(HTTP_PORT, () => console.log(`Listening on ${HTTP_PORT}...`))
